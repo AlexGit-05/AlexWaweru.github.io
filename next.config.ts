2927ed2443f51bf73b93e13654f5868ef749a2ac
Alex <106592582+AlexGit-05@users.noreply.github.com>
2024-12-19 10:37:37 +0300

    Update next.config.ts
    add an output: "export"

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Export the app as a static site
 };
 
 export default nextConfig;
