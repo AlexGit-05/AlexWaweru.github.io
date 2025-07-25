92acd4385454e711e68d0c2586d6c1c7a5deb469
Alex <alexwawerugit@gmail.com>
2025-07-25 10:24:38 +0300

    updated footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="bg-gray-900 py-6">
       <div className="container mx-auto px-6 text-center">
-        <p>&copy; {new Date().getFullYear()} Alex Waweru. All rights reserved. | Data Scientist & ML Engineer</p>
+        <p>&copy; {new Date().getFullYear()} Alex Waweru. All rights reserved. | Data Scientist & AI Engineer</p>
       </div>
     </footer>
   )
